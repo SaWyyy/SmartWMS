3c25994ed956f069062cea720bd3bba5ecff0909
SaWyyy <kamilsadowski001@gmail.com>
2024-08-22 16:47:51 +0200

    Add Registration model

---
+++ Models/Registration.cs
@@ -0,0 +1,10 @@
+namespace SmartWMS.Models;
+
+public class Registration
+{
+    public string Email { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    public string ManagerId { get; set; }
+}
