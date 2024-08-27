082eb9713f791830e2d0a3bc4045008d7a72dc65
Dawid Rutkowski <dawidrut01@gmail.com>
2024-08-27 19:28:36 +0200

    Add validation for Shelves fields

--- Models/DTOs/ShelfDto.cs
+++ Models/DTOs/ShelfDto.cs
@@ -1,19 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 using SmartWMS.Models.Enums;
 
 namespace SmartWMS.Models;
 
 public class ShelfDto
 {
-    public int WarehouseLocalizationId { get; set; }
 
+    [RegularExpression("^[A-D][0-9]{1,2}$", ErrorMessage = "Lane must contain capital letter and number, 2 digit at most.")]
     public string Lane { get; set; } = null!;
 
+    [Range(1, int.MaxValue, MinimumIsExclusive = false, ErrorMessage = "Rack numeration must start from 1")]
     public int Rack { get; set; }
 
+    [Range(0, 4, ErrorMessage = "Level must be between 0 and 4")]
     public LevelType Level { get; set; }
-
+    
+    [Range(0, int.MaxValue, MinimumIsExclusive = true, ErrorMessage = "Quantity cant be less than 1")]
     public int MaxQuant { get; set; }
 
+    [Range(0, int.MaxValue, MinimumIsExclusive = false, ErrorMessage = "Quantity cant be less than 0")]
     public int CurrentQuant { get; set; }
 
     public int? ProductsProductId { get; set; }
