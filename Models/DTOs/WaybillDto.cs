06e47b38024b6a2a916ed3f8916d1b4c5b987a2a
SaWyyy <kamilsadowski001@gmail.com>
2024-08-29 15:53:04 +0200

    Add Waybill validation

--- Models/DTOs/WaybillDto.cs
+++ Models/DTOs/WaybillDto.cs
@@ -4,15 +4,19 @@ namespace SmartWMS.Models;
 
 public class WaybillDto
 {
-
+    [DataType(DataType.DateTime)]
     public DateTime ShippingDate { get; set; }
 
+    [DataType(DataType.DateTime)]
     public DateTime LoadingDate { get; set; }
-
+    
+    [Range(1, int.MaxValue, ErrorMessage = "Must be integer value grater than 0")]
     public int CountriesCountryId { get; set; }
-
+    
+    [RegularExpression("^[0-9]{2}-[0-9]{3}\n", ErrorMessage = "Pattern is: XX-XXX")]
     public string PostalCode { get; set; } = null!;
-
+    
+    [MaxLength(45, ErrorMessage = "Supplier name is too long")]
     public string SupplierName { get; set; } = null!;
 
 
