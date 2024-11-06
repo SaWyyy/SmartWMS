72d2edf9b6ba86adbef9831ba1ae4b8d6e5f78a8
SaWyyy <kamilsadowski001@gmail.com>
2024-11-06 17:01:32 +0100

    Add lane controller

---
+++ SmartWMS/Controllers/LaneController.cs
@@ -0,0 +1,90 @@
+using Microsoft.AspNetCore.Mvc;
+using SmartWMS.Models.DTOs;
+using SmartWMS.Repositories.Interfaces;
+
+namespace SmartWMS.Controllers;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class LaneController : ControllerBase
+{
+    private readonly ILaneRepository _repository;
+    private readonly ILogger<LaneController> _logger;
+
+    public LaneController(ILaneRepository repository, ILogger<LaneController> logger)
+    {
+        this._repository = repository;
+        this._logger = logger;
+    }
+
+    public async Task<IActionResult> AddLane(LaneDto dto)
+    {
+        try
+        {
+            var result = await _repository.Add(dto);
+            _logger.LogInformation($"Lane with id: {result.LaneId} has been added");
+            
+            return Ok($"Lane with id: {result.LaneId} has been added");
+        }
+        catch (SmartWMSExceptionHandler e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(e.Message);
+        }
+    }
+
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _repository.GetAll();
+
+        return Ok(result);
+    }
+
+    public async Task<IActionResult> Get(int id)
+    {
+        try
+        {
+            var result = await _repository.Get(id);
+            _logger.LogInformation("Lane found");
+
+            return Ok(result);
+        }
+        catch (SmartWMSExceptionHandler e)
+        {
+            _logger.LogError(e.Message);
+            return NotFound(e.Message);
+        }
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            var result = await _repository.Delete(id);
+            _logger.LogInformation("Lane deleted");
+
+            return Ok($"Lane with id: {result.LaneId} has been removed");
+        }
+        catch (SmartWMSExceptionHandler e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(e.Message);
+        }
+    }
+
+    public async Task<IActionResult> Update(int id, LaneDto dto)
+    {
+        try
+        {
+            var result = await _repository.Update(id, dto);
+            _logger.LogInformation($"Lane updated");
+
+            return Ok($"Lane with id: {result.LaneId} has been updated");
+        }
+        catch (SmartWMSExceptionHandler e)
+        {
+            _logger.LogError(e.Message);
+            return BadRequest(e.Message);
+        }
+    }
+}
