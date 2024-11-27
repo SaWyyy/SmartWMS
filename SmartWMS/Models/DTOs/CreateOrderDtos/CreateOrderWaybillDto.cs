fe13585cb3b0e2db1ce42a54289e5f3ef4e0c1cb
r00j3K <adrianr8@o2.pl>
2024-11-27 12:32:32 +0100

    Change PostalCode's pattern for validation

--- SmartWMS/Models/DTOs/CreateOrderDtos/CreateOrderWaybillDto.cs
+++ SmartWMS/Models/DTOs/CreateOrderDtos/CreateOrderWaybillDto.cs
@@ -6,7 +6,7 @@ public class CreateOrderWaybillDto
 {
     [Range(0, int.MaxValue)]
     public int CountryId { get; set; }
-    [RegularExpression("^[0-9]{2}-[0-9]{3}$", ErrorMessage = "Pattern is: XX-XXX")]
+    [RegularExpression("^[0-9]{2}[0-9]{3}$", ErrorMessage = "Pattern is: XXXXX")]
     public string PostalCode { get; set; }
     
     [MinLength(3, ErrorMessage = "Supplier name must have at least 3 characters")]
