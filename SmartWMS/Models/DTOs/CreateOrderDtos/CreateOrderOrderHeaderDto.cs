4817676e2ab834c04ac3c0dcb4846bd6772ec0c8
r00j3K <adrianr8@o2.pl>
2024-11-27 12:40:28 +0100

    Change validation rules for DestinationAddress in CreateOrderOrderHeaderDto

--- SmartWMS/Models/DTOs/CreateOrderDtos/CreateOrderOrderHeaderDto.cs
+++ SmartWMS/Models/DTOs/CreateOrderDtos/CreateOrderOrderHeaderDto.cs
@@ -4,7 +4,7 @@ namespace SmartWMS.Models.DTOs.CreateOrderDtos;
 
 public class CreateOrderOrderHeaderDto
 {
-    [RegularExpression("^([A-ZŁŚŹŻ][A-Za-zĄĆĘŁŃÓŚŹŻąćęłńóśźż]{2,}\\s){1,5}\\d{1,5}(([A-Za-z](/\\d{1,5})?)|(/\\d{1,5}))?$",
+    [RegularExpression("^[A-Za-zĄąĆćĘęŁłŃńÓóŚśŹźŻż]{2,20},\\s([A-ZŁŚŹŻ][A-Za-zĄĆĘŁŃÓŚŹŻąćęłńóśźż]{2,}\\s){1,5}\\d{1,5}(([A-Za-z](/\\d{1,5})?)|(/\\d{1,5}))?$",
         ErrorMessage = "Bad pattern")]
     [MaxLength(45, ErrorMessage = "Address too long")]
     public string DestinationAddress { get; set; }
