8a30d07e451d6c5a26285f10d1c5de45924ab348
r00j3K <adrianr8@o2.pl>
2024-11-27 12:48:04 +0100

    Add migrations' files

---
+++ SmartWMS/Migrations/20241127114254_ChangeMaxLengthForDestinationAddressField.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartWMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeMaxLengthForDestinationAddressField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "destination_address",
+                table: "order_headers",
+                type: "character varying(67)",
+                maxLength: 67,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(65)",
+                oldMaxLength: 65);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "destination_address",
+                table: "order_headers",
+                type: "character varying(65)",
+                maxLength: 65,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(67)",
+                oldMaxLength: 67);
+        }
+    }
+}
